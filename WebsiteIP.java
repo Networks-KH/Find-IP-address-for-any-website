beb917e96e33c9e1b444638862d88c605e68d6c3
Khalid Al-Ammar <105190612+Networks-KH@users.noreply.github.com>
2023-07-06 01:54:59 +0300

    Update WebsiteIP.java

--- WebsiteIP.java
+++ WebsiteIP.java
@@ -6,10 +6,11 @@ class WebsiteIP
     public static void main(String[] args) throws Exception{
         InetAddress obj1;
         Scanner obj2 = new Scanner(System.in);
-        System.out.println("Write the Web page name with domain: ");
+        System.out.println("Write the Web page name with domain like this format 'www.WebsiteName.com' : ");
         String webName = obj2.nextLine();
         obj1 = InetAddress.getByName(webName);
         System.out.println("The IP for this website is: "+obj1);
+        //Here is another function, it is give you the IP your device 
         obj1 = InetAddress.getLocalHost();
         System.out.println("Your local IP is: "+obj1);
      
